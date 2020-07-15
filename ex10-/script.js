1a3a0d22fecab2653c17aae9bd328be8e383d78c
tekubambam <verdierem@gmail.com>
2020-07-15 20:11:07 +0200

    modifié :         ex10-/index.html 	modifié :         ex10-/script.js

--- ex10-/script.js
+++ ex10-/script.js
@@ -1,8 +1,51 @@
 "use strict"
-// afficher les nombres de 0 à 10 et leurs carrés
 
-// afficher les nombres de 0 à 16 et leurs carrés
+let start = 0;
+let n = 100 ;
 
-// déclarer une variable n, afficher les nombres de 0 à n et leurs carrés
+do {
+    console.log(start, start**2)
+    start += 1
+  } while (start <= n)
 
-// déclarer une variable start, afficher les nombres de start à n et leurs carrés
+
+
+/*let number = -1
+
+while (number++ < 10) {
+   console.log(number, number**2);
+}
+
+afficher les nombres de 0 à 10 et leurs carrés
+Base 10
+
+let number = 0;
+
+do {
+  console.log(number, number**2)
+  number++
+} while (number <= 10)
+
+
+afficher les nombres de 0 à 16 et leurs carrés
+let number = 0;
+
+do {
+  console.log(number, number**2)
+  number++
+} while (number <= 16)
+
+let number = 0;
+let n = 100 ;
+
+do {
+    console.log(number, number**2)
+    number++
+  } while (number <= n)
+
+
+déclarer une variable n, afficher les nombres de 0 à n et leurs carrés
+
+déclarer une variable start, afficher les nombres de start à n et leurs carrés
+$
+*/ 
